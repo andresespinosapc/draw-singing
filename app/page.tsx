2a804510fa49ed0169124ff89aff5da81e4ff35b
Andres Espinosa <andresespinosapc@gmail.com>
2023-05-28 17:17:40 -0400

    feat: add guitar tuner

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,165 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import AudioContext from "./contexts/AudioContext";
+import autoCorrelate from "./libs/AutoCorrelate";
+import {
+  noteFromPitch,
+  centsOffFromPitch,
+  getDetunePercent,
+} from "./libs/Helpers";
+
+const audioCtx = AudioContext.getAudioContext();
+const analyserNode = AudioContext.getAnalyser();
+const buflen = 2048;
+var buf = new Float32Array(buflen);
+
+const noteStrings = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+];
+
+export default function App() {
+  const [source, setSource] = useState(null);
+  const [started, setStart] = useState(false);
+  const [pitchNote, setPitchNote] = useState("C");
+  const [pitchScale, setPitchScale] = useState("4");
+  const [pitch, setPitch] = useState("0 Hz");
+  const [detune, setDetune] = useState("0");
+  const [notification, setNotification] = useState(false);
+
+  const updatePitch = (time) => {
+    analyserNode.getFloatTimeDomainData(buf);
+    var ac = autoCorrelate(buf, audioCtx.sampleRate);
+    if (ac > -1) {
+      let note = noteFromPitch(ac);
+      let sym = noteStrings[note % 12];
+      let scl = Math.floor(note / 12) - 1;
+      let dtune = centsOffFromPitch(ac, note);
+      setPitch(parseFloat(ac).toFixed(2) + " Hz");
+      setPitchNote(sym);
+      setPitchScale(scl);
+      setDetune(dtune);
+      setNotification(false);
+      console.log(note, sym, scl, dtune, ac);
+    }
+  };
+
+  useEffect(() => {
+    if (source != null) {
+      source.connect(analyserNode);
+    }
+  }, [source]);
+
+  setInterval(updatePitch, 1);
+
+  const start = async () => {
+    const input = await getMicInput();
+
+    if (audioCtx.state === "suspended") {
+      await audioCtx.resume();
+    }
+    setStart(true);
+    setNotification(true);
+    setTimeout(() => setNotification(false), 5000);
+    setSource(audioCtx.createMediaStreamSource(input));
+  };
+
+  const stop = () => {
+    source.disconnect(analyserNode);
+    setStart(false);
+  };
+
+  const getMicInput = () => {
+    return navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        autoGainControl: false,
+        noiseSuppression: false,
+        latency: 0,
+      },
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex justify-center items-center h-screen">
+      <div
+        className={
+          notification
+            ? "visible transition-all fixed top-0 bg-gray-400 text-white w-10/12 text-xs md:text-sm text-center py-4 mt-2 rounded-full shadow-2xl"
+            : "invisible fixed top-0"
+        }
+      >
+        Please, bring your instrument near to the microphone!
+      </div>
+      <div className="flex flex-col items-center">
+        <div
+          className={
+            started
+              ? "visible flex flex-col transition-all ease-in delay-75 bg-gray-200 justify-center items-center p-10 rounded-xl shadow-lg mb-5 w-60 h-60"
+              : "invisible transition-all w-0 h-0"
+          }
+        >
+          <div className="flex items-start font-mono">
+            <span
+              className={
+                started
+                  ? "visible transition-all delay-75 font-thin text-9xl"
+                  : "invisible text-xs"
+              }
+            >
+              {pitchNote}
+            </span>
+            <span className="bg-green-600 p-1 px-2 text-white rounded-lg">
+              {pitchScale}
+            </span>
+          </div>
+
+          <div className="w-full flex justify-center items-center">
+            <div
+              className="bg-gradient-to-r to-green-400 from-red-600 py-1 rounded-full rotate-180"
+              style={{
+                width: (detune < 0 ? getDetunePercent(detune) : "50") + "%",
+              }}
+            ></div>
+            <span className="font-bold text-lg text-green-800">I</span>
+            <div
+              className="bg-gradient-to-r from-green-400 to-red-600 py-1 rounded-full"
+              style={{
+                width: (detune > 0 ? getDetunePercent(detune) : "50") + "%",
+              }}
+            ></div>
+          </div>
+          <div className="mt-2 text-xs text-gray-400">
+            <span>{pitch}</span>
+          </div>
         </div>
+        {!started ? (
+          <button
+            className="bg-red-600 text-white w-20 h-20 rounded-full shadow-xl transition-all"
+            onClick={start}
+          >
+            Start
+          </button>
+        ) : (
+          <button
+            className="bg-red-800 text-white w-20 h-20 rounded-full shadow-xl transition-all"
+            onClick={stop}
+          >
+            Stop
+          </button>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
